9129a097beb3b9d205ec721a4d5d0dc05fa206bf
dusainbolt <dule9xxpro@gmail.com>
2020-09-11 22:44:24 +0700

    fix rep comment

--- src/components/News/Comment.jsx
+++ src/components/News/Comment.jsx
@@ -14,6 +14,7 @@ import Input from "../Input";
 import logo1 from "../../common/image/LogoSidebar.png";
 import { Formik, Field } from "formik";
 import { REP_COMMENT } from "../../common";
+import { Link } from "react-router-dom";
 function CommentPost({ className = "" }) {
   const [likes, setLikes] = useState(0);
   const [action, setAction] = useState(null);
@@ -24,7 +25,10 @@ function CommentPost({ className = "" }) {
   };
 
   const initialVales = { message: "" };
-
+  const [repCommentId, setCommentId] = useState(0);
+  const changeRepCommentId = id => () => {
+    setCommentId(id);
+  };
   const actions = [
     <Tooltip key="comment-basic-like" title="Like">
       <span onClick={like}>
@@ -32,61 +36,63 @@ function CommentPost({ className = "" }) {
         <span className="comment-action">{likes}</span>
       </span>
     </Tooltip>,
-    <span key="comment-basic-reply-to">Reply to</span>,
+    <span onClick={changeRepCommentId(1)} key="comment-basic-reply-to">Trả lời</span>,
     <span key="comment-basic-reply-to">20 gio</span>,
   ];
+  
 
   const renderCommentItem = (child, type) => {
     console.log(type);
     return (
       <>
-      <Comment
-        actions={actions}
-        author={<a>Han Solo</a>}
-        avatar={
-          <img
-            src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-            alt="Han Solo"
-          />
-        }
-        content={
-          <p>
-            "Chắc hẳn anh em khi tham gia dự án, cũng đều nghe về khái niệm UI/UX rồi đúng không ?
-            Nhưng đối với một số bạn mới đi làm, và Inputcó thể đang chập chững bước vào nghề thì có
-            thể cũng chưa nghe qua về khái niệm này.
-          </p>
-        }
-      >
-        {child}
-        {child}
-        {child}
-        {child}
-        {type ===  REP_COMMENT && <Formik validationSchema={validateMessage} initialValues={initialVales}>
-        {formik => (
-          <div className="form-feed__bottom">
+        <Comment
+          actions={actions}
+          author={<Link>Du sainbolt</Link>}
+          avatar={
             <img
-              className="form-feed__bottom--img"
-              src="https://www.w3schools.com/howto/img_avatar.png"
-              alt="avatar"
+              src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+              alt="Han Solo"
             />
-            <Field
-              name="message"
-              component={Input}
-              maxLength={255}
-              showError={false}
-              className="form-feed__bottom--comment"
-              placeholder={"Enter message..."}
-              autoSize
-              onPressEnter={formik.handleSubmit}
-              Icon={SendOutlined}
-              callHandleIcon={formik.handleSubmit}
-              type="textarea"
-            />
-          </div>
-        )}
-      </Formik>
-    }
-      </Comment>
+          }
+          content={
+            <p>
+              "Chắc hẳn anh em khi tham gia dự án, cũng đều nghe về khái niệm UI/UX rồi đúng không ?
+              Nhưng đối với một số bạn mới đi làm, và Inputcó thể đang chập chững bước vào nghề thì
+              có thể cũng chưa nghe qua về khái niệm này.
+            </p>
+          }
+        >
+          {child}
+          {child}
+          {child}
+          {child}
+          {type === REP_COMMENT && repCommentId !== 0 && (
+            <Formik validationSchema={validateMessage} initialValues={initialVales}>
+              {formik => (
+                <div className="form-feed__bottom">
+                  <img
+                    className="form-feed__bottom--img"
+                    src="https://www.w3schools.com/howto/img_avatar.png"
+                    alt="avatar"
+                  />
+                  <Field
+                    name="message"
+                    component={Input}
+                    maxLength={255}
+                    showError={false}
+                    className="form-feed__bottom--comment"
+                    placeholder={"Enter message..."}
+                    autoSize
+                    onPressEnter={formik.handleSubmit}
+                    Icon={SendOutlined}
+                    callHandleIcon={formik.handleSubmit}
+                    type="textarea"
+                  />
+                </div>
+              )}
+            </Formik>
+          )}
+        </Comment>
       </>
     );
   };
